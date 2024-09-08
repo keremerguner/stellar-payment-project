b2c51971306fa9668c46ccd3f35e93f45f8f9fd1
Kerem Erguner <73108947+keremerguner@users.noreply.github.com>
2024-09-08 15:30:31 +0300

    transfer işlemine kısa mesaj MEMO eklendi

--- src/transfer.js
+++ src/transfer.js
@@ -2,25 +2,30 @@ var StellarSdk = require("stellar-sdk");
 var server = new StellarSdk.Horizon.Server(
   "https://horizon-testnet.stellar.org"
 );
-var sourceKeys = StellarSdk.Keypair.fromSecret(
-  "SA2INPQH3H3NA4JIK4FV5DJGGSKWIF6OMZQVGMX44OCSLSOG7H75M7BF"
-); // Buraya kendi secret key'ini yaz
-var destinationId = "GCBB6P6WZEIOJL2ZR4WQEN7OAM47W7LTS5HSOPCSZLBAG3SOQOAMHEST"; // Buraya hedef public key'i yaz
 
-// İlk olarak hedef hesabın mevcut olup olmadığını kontrol ediyoruz
+var sourceKeys = StellarSdk.Keypair.fromSecret(
+  "SA2INPQH3H3NA4JIK4FV5DJGGSKWIF6OMZQVGMX44OCSLSOG7H75M7BF" // Kaynak hesabın secret key'i
+);
+var destinationId = "GCBB6P6WZEIOJL2ZR4WQEN7OAM47W7LTS5HSOPCSZLBAG3SOQOAMHEST"; // Hedef hesabın public key'i
+
+var transaction;
+var memoText = "Test Transfer!"; // Memo mesajı 28 karakterden kısa
+
+// İlk olarak hedef hesabın var olup olmadığını kontrol ediyoruz.
 server
   .loadAccount(destinationId)
   .catch(function (error) {
     if (error instanceof StellarSdk.NotFoundError) {
-      throw new Error("The destination account does not exist!");
+      throw new Error("Hedef hesap bulunamadı!");
     } else return error;
   })
+  // Eğer hata yoksa, kaynak hesap bilgilerini yüklüyoruz.
   .then(function () {
     return server.loadAccount(sourceKeys.publicKey());
   })
   .then(function (sourceAccount) {
-    // İşlem oluşturuluyor
-    var transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
+    // İşlem oluşturuyoruz.
+    transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
       fee: StellarSdk.BASE_FEE,
       networkPassphrase: StellarSdk.Networks.TESTNET,
     })
@@ -28,22 +33,22 @@ server
         StellarSdk.Operation.payment({
           destination: destinationId,
           asset: StellarSdk.Asset.native(),
-          amount: "10", // Gönderilecek XLM miktarı
+          amount: "10", // Gönderilecek miktar
         })
       )
-      .addMemo(StellarSdk.Memo.text("Test Transaction")) // Memo ekliyoruz
+      .addMemo(StellarSdk.Memo.text(memoText)) // Memo ekliyoruz
       .setTimeout(180)
       .build();
-    
-    // İşlemi imzalıyoruz
+
+    // İşlemi imzalıyoruz.
     transaction.sign(sourceKeys);
 
-    // İşlemi Stellar ağına gönderiyoruz
+    // İşlemi gönderiyoruz.
     return server.submitTransaction(transaction);
   })
   .then(function (result) {
-    console.log("Success! Results:", result);
+    console.log("Başarılı! Sonuç:", result);
   })
   .catch(function (error) {
-    console.error("Something went wrong!", error);
+    console.error("Bir hata oluştu!", error);
   });
